7e3f89771eeb75ada54c85a8c14d02d8058e89e0
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-02-16 01:13:56 -0500

    t

--- model/backbone/mymodel_moreview.py
+++ model/backbone/mymodel_moreview.py
@@ -203,8 +203,7 @@ class ViewNet(nn.Module):
         
         print("\n")
         print("test size:")
-        print(gl2[1])
-        print(len(gl), "," , len(gl2[1]))
+        print(len(gl), "," , len(gl2[1]), ",", gl2[1] == gl2[0])
         print("\n")
 
         x = self.set_layer3(x) # 40,64,16,16
