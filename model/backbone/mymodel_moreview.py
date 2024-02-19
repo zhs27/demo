99e3522e9a80d3752526fae20589e9b908c92ce1
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-02-19 11:48:05 -0500

    test

--- model/backbone/mymodel_moreview.py
+++ model/backbone/mymodel_moreview.py
@@ -34,15 +34,15 @@ class SetBlock(nn.Module):
         self.forward_block = forward_block
         self.pooling = pooling
         if pooling:
-            #self.pool2d = nn.MaxPool2d(2)
+            self.pool2d = nn.MaxPool2d(2)
             #wablock replacement
-            self.pool2d = wa_module.wa_module()
+            #self.pool2d = wa_module.wa_module()
     def forward(self, x):
         n, s, c, h, w = x.size()
         x = self.forward_block(x.view(-1,c,h,w))
         if self.pooling:
             x = self.pool2d(x)
-            x = x[0]
+            #x = x[0]
         _, c, h, w = x.size()
         return x.view(n, s, c, h ,w)
 
