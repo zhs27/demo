c6f5e80d914ca1190d119fdfd80b1fd072733002
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-02-20 21:21:42 -0500

    network change

--- model/backbone/mymodel_moreview.py
+++ model/backbone/mymodel_moreview.py
@@ -144,6 +144,7 @@ class ViewNet(nn.Module):
         self.gl_layer2 = BasicConv2d(_gl_channels[0], _gl_channels[0], 3, padding=1)
         self.gl_layer3 = BasicConv2d(_gl_channels[0], _gl_channels[1], 3, padding=1)
         self.gl_layer4 = BasicConv2d(_gl_channels[1], _gl_channels[1], 3, padding=1)
+        self.gl_layer5 = nn.ConvTranspose2d(64, 64, 3, stride=2, padding=1)
         self.gl_pooling = nn.MaxPool2d(2)
         # Replace max pooling by wavelet block
         self.gl_pooling2 = wa_module.wa_module()
@@ -199,13 +200,14 @@ class ViewNet(nn.Module):
         gl = self.gl_layer1(self.vp1(x))
         # gl = self.gl_layer1(self.frame_max(x)[0]) # x's shape is (bs,64,32,32)
         
-        gl = self.gl_layer2(gl) # just normal convolutional network
+        #gl = self.gl_layer2(gl) # just normal convolutional network
         gl = self.gl_pooling2(gl)[0] 
+        gl = self.gl_layer2(gl) 
         #gl2 = self.gl_pooling2(gl)
         #gl = self.gl_pooling(gl) # the shape is (40,64,16,16)
         
         #print("\n")
-        #print("test size:")
+        #print("test size:")    
         #print(len(gl), "," , len(gl2[1]))
         #print("\n")
 
@@ -213,9 +215,17 @@ class ViewNet(nn.Module):
         x = self.set_layer4(x)
         
         # gl = self.gl_layer3(gl + self.frame_max(x)[0])
-        gl = self.gl_layer3(gl + self.vp2(x))
-        
+        gl = gl + self.vp2(x)
+        print(gl.size())
+        gl2 = self.gl_pooling2(gl)[0]
+        print(gl2.size())
+        gl2 = self.gl_layer5(gl)
+        print(gl2.size())
+        gl = self.gl_layer3(gl)
+        print(gl.size())
         gl = self.gl_layer4(gl)
+        print(gl.size())
+
 
         x = self.set_layer5(x)
         x = self.set_layer6(x)
