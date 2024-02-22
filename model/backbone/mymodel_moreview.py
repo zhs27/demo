cda048f5aebb2e47aec24c6c2d69ebd956c2d754
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-02-21 20:39:38 -0500

    fix

--- model/backbone/mymodel_moreview.py
+++ model/backbone/mymodel_moreview.py
@@ -144,7 +144,7 @@ class ViewNet(nn.Module):
         self.gl_layer2 = BasicConv2d(_gl_channels[0], _gl_channels[0], 3, padding=1)
         self.gl_layer3 = BasicConv2d(_gl_channels[0], _gl_channels[1], 3, padding=1)
         self.gl_layer4 = BasicConv2d(_gl_channels[1], _gl_channels[1], 3, padding=1)
-        self.gl_layer5 = nn.ConvTranspose2d(64, 64, 3, stride=2, padding=1)
+        self.gl_layer5 = nn.ConvTranspose2d(64, 64, 2, stride=2, padding=1)
         self.gl_pooling = nn.MaxPool2d(2)
         # Replace max pooling by wavelet block
         self.gl_pooling2 = wa_module.wa_module()
@@ -202,8 +202,7 @@ class ViewNet(nn.Module):
         
         #gl = self.gl_layer2(gl) # just normal convolutional network
         gl = self.gl_pooling2(gl)[0] 
-        gl = self.gl_layer2(gl)
-        print(gl.size()) 
+        gl = self.gl_layer2(gl) 
         #gl2 = self.gl_pooling2(gl)
         #gl = self.gl_pooling(gl) # the shape is (40,64,16,16)
         
