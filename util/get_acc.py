3f37baf4102b8c887e0ef22d96628015e85a3cb1
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-02-22 21:51:02 -0500

    test

--- util/get_acc.py
+++ util/get_acc.py
@@ -12,4 +12,6 @@ def cal_cfm(pred,label,typeacc,ncls):
         typeacc[0,label[i]] += 1
         if pred[i] == label[i]:
             typeacc[1, label[i]] += 1 
-    return cfm
+    print(typeacc)
+    print(cfm)
+    return cfm,typeacc
