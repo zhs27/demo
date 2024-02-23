dca886eb61f8509149726ef93cc1ac76ead1ce04
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-02-23 08:34:32 -0500

    t

--- model/backbone/mymodel_moreview.py
+++ model/backbone/mymodel_moreview.py
@@ -194,10 +194,9 @@ class ViewNet(nn.Module):
         norm_img=self.get_img(inpt) # (20,6,128,128)
         #norm_img.save()
         norm_img=norm_img.unsqueeze(2)
-        
+        print(norm_img[10, 1, :, :])
         
         x=self.set_layer1(norm_img)
-        print(x)
         x=self.set_layer2(x)
         
         gl = self.gl_layer1(self.vp1(x))
