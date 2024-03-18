f9a735d36f6408b0d65e013b69403712d5f07132
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-03-18 15:01:51 -0400

    1

--- main_imageinpt.py
+++ main_imageinpt.py
@@ -42,7 +42,7 @@ def get_arg():
     cfg.add_argument('--k_way',default=5,type=int)
     cfg.add_argument('--n_shot',default=1,type=int)
     cfg.add_argument('--query',default=10,type=int)
-    cfg.add_argument('--backbone',default='ViewNetimg',choices=['dgcnn','mv','gaitset','ViewNet','Point_Trans'])
+    cfg.add_argument('--backbone',default='ViewNetimg',choices=['ViewNetimg','dgcnn','mv','gaitset','ViewNet','Point_Trans'])
     cfg.add_argument('--fs_head',type=str,default='Trip_CIA',choices=['protonet','cia','trip','pv_trip','Trip_CIA','MetaOp','Relation'])
     cfg.add_argument('--fold',default=0,type=int)
     # ===================================#
