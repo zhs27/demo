57b8816c71a399a7c8c78bc441e72aa6208b68e6
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-03-29 17:56:10 -0400

    1

--- cartoonx.py
+++ cartoonx.py
@@ -100,11 +100,12 @@ class CartoonX:
         # Initialize mask on wavelet coefficients yl and yh
         m_yl, m_yh = self.get_init_mask(yl, yh)
         
-        '''
+        
         # Get total number of mask entries
         with torch.no_grad():
             num_mask = 0
             num_mask = m_yl.view(m_yl.size(0), -1).size(-1) + sum([m.view(m.size(0), -1).size(-1) for m in m_yh])
+        '''
         
         # Initialize optimizer
         opt = torch.optim.Adam([m_yl]+m_yh, lr=self.lr)
