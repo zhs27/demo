a7e86a74a3ac5664230c08986aa48c7b5e015329
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-03-31 13:24:07 -0400

    1

--- cartoonx.py
+++ cartoonx.py
@@ -78,7 +78,9 @@ class CartoonX:
         for i in x:
             y1,y2 = self.forward_dwt(i)
             self.compute_obfuscation_strategy(y1, y2)
-            m_y1,m_y2 = self.get_init_mask(y1,y2) 
+            m_y1,m_y2 = self.get_init_mask(y1,y2)
+            m_y1 = torch.stack(m_y1)
+            m_y2 = torch.stack(m_y2)
             optpara.append([m_y1] + m_y2)
             yl.append(y1)
             yh.append(y2)
