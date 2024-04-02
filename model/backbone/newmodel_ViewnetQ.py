69f5b2bdbc8dcce80e37142408919eea9859e0eb
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-04-02 11:16:55 -0400

    1

--- model/backbone/newmodel_ViewnetQ.py
+++ model/backbone/newmodel_ViewnetQ.py
@@ -196,7 +196,7 @@ class ViewNetpt(nn.Module):
         128 is the image size
         '''
         CARTOONX_HPARAMS = {
-        "l1lambda": 10., "lr": 1e-1, 'obfuscation': 'gaussian',
+        "l1lambda": 30., "lr": 1e-1, 'obfuscation': 'gaussian',
         "maximize_label": True, "optim_steps": 3,  
         "noise_bs": 1, 'mask_init': 'ones'
         } 
