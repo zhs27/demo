4ed9dc38629b1eb413f590cd03487f441a36d155
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-04-06 11:46:21 -0400

    test

--- testCartoonX.py
+++ testCartoonX.py
@@ -171,8 +171,8 @@ def main(cfg):
 
     # Hparams WaveletX with spatial reg
     CARTOONX_HPARAMS = {
-        "l1lambda": 2., "lr": 1e-1, 'obfuscation': 'gaussian',
-        "maximize_label": True, "optim_steps": 7,  
+        "l1lambda": 0.1, "lr": 1e-1, 'obfuscation': 'gaussian',
+        "maximize_label": True, "optim_steps": 50,  
         "noise_bs": 1, 'mask_init': 'ones'
     } 
 
